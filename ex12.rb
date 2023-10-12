7df528aacb57a7d531354162942b9d52400653f3
kazvee <kayvee123@gmail.com>
2023-10-11 21:49:12 -0600

    Add Exercise 12

---
+++ ex12.rb
@@ -0,0 +1,14 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# Could use the `to_f` method, which converts a value to a floating-point number
