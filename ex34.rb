c0e7aba312860d6e0f6e185b030381b770a80ab4
kazvee <kayvee123@gmail.com>
2023-10-12 10:47:25 -0600

    Add Exercise 34

---
+++ ex34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1
+puts "The animal at 1: #{animals[1]}."
+
+# The third (3rd) animal
+puts "The third (3rd) animal: #{animals[2]}."
+
+# The first (1st) animal
+puts "The first (1st) animal: #{animals[0]}."
+
+# The animal at 3
+puts "The animal at 3: #{animals[3]}."
+
+# The fifth (5th) animal
+puts "The fifth (5th) animal: #{animals[4]}."
+
+# The animal at 2
+puts "The animal at 2: #{animals[2]}."
+
+# The sixth (6th) animal
+puts "The sixth (6th) animal: #{animals[5]}."
+
+# The animal at 4
+puts "The animal at 4: #{animals[4]}."
